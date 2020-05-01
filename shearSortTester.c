d3df70ba5d7f06ba3c4a1efe6e61a4d2864d65fd
Omer <omersch381@gmail.com>
2020-05-01 15:04:29 +0300

    cleaner code

--- shearSortTester.c
+++ shearSortTester.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+#define FILE_NAME "input.dat"
 #define TAG 0
 #define MIN_TO_MAX 0
 #define MAX_TO_MIN 1
@@ -14,30 +15,35 @@
 #define ROW 1
 #define COL 0
 
+struct
+{
+    int id;
+    int length;
+    int width;
+    int height;
+} typedef Box;
+
 int floorSqrt(int x);
-void compare(float *myValue, float *otherValue, int direction);
+void compare(Box *myBox, Box *otherBox, int direction);
 void sanityCheck(int numOfProcesses, int n);
 MPI_Comm getNewCommunicator(int n);
 void getCoordinatesFromGivenRank(MPI_Comm newCommunicator, int rank, int *rowIndex, int *colIndex);
 int getRankFromGivenCoordinates(MPI_Comm newCommunicator, int *rowIndex, int *colIndex);
 void getNeighborsRanksFromGivenRank(MPI_Comm newCommunicator, int rank, int *pRightRank, int *pLeftRank, int direction);
-void shearSort(MPI_Comm newCommunicator, int rank, int rowIndex, int colIndex, float myStruct[], int n);
-void swapFloats(float *myValue, float *otherValue);
-void sortUnit(MPI_Comm newCommunicator, int rank, float myStruct[], int n, int row, int direction, int unit);
-void sortUnits(MPI_Comm newCommunicator, int rank, int rowIndex, int colIndex, float myStruct[], int n, int unit);
-void oddEvenSortGeneral(float *myValue, int n, int pSourceRank, int pTargetRank, int location, int direction, int rank, int unit);
+void shearSort(MPI_Comm newCommunicator, int rank, int rowIndex, int colIndex, Box *myStruct, int n);
+void swapBoxes(Box *myBox, Box *otherBox);
+void sortUnit(MPI_Comm newCommunicator, int rank, Box *myStruct, int n, int row, int direction, int unit);
+void sortUnits(MPI_Comm newCommunicator, int rank, int rowIndex, int colIndex, Box *myStruct, int n, int unit);
+void oddEvenSortGeneral(Box *myStruct, int n, int pSourceRank, int pTargetRank, int location, int direction, int rank, int unit);
+void printBox(Box *givenBox, int rank);
 
 int main(int argc, char *argv[])
 {
     int rank, numOfProcesses;
     MPI_Comm newCommunicator;
-    int numOfDimensionsArray[2], isPeriodicArray[2], reorder;
-    int coordinatesArray[2], newId;
     int rowIndex, colIndex;
-    int pLeftRank, pRightRank;
+
     int n;
-    float volume, height;
-    float myStruct[2];
 
     MPI_Init(&argc, &argv);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
@@ -47,119 +53,69 @@ int main(int argc, char *argv[])
 
     sanityCheck(numOfProcesses, n);
 
-    height = rank;     //test
-    volume = 2 * rank; //test
-    myStruct[0] = volume;
-    myStruct[1] = height;
-    // until here - we received the structs
+    FILE *input_file;
+    Box input;
+    Box myStruct;
+    Box boxesArray[numOfProcesses];
+    int data = 0, i;
+    if (rank == 0) //master reads the file
+    {
+        input_file = fopen(FILE_NAME, "r");
+        if (input_file == NULL)
+        {
+            fprintf(stderr, "\nError opening file\n");
+            exit(1);
+        }
+        while (fread(&input, sizeof(Box), 1, input_file))
+        {
+            boxesArray[i] = input;
+            i++;
+        }
+        fclose(input_file);
+    }
+    MPI_Scatter(boxesArray, sizeof(myStruct), MPI_BYTE, &myStruct, sizeof(myStruct), MPI_BYTE, 0, MPI_COMM_WORLD);
+
     newCommunicator = getNewCommunicator(n);
 
     getCoordinatesFromGivenRank(newCommunicator, rank, &rowIndex, &colIndex);
 
     rank = getRankFromGivenCoordinates(newCommunicator, &rowIndex, &colIndex);
 
-    shearSort(newCommunicator, rank, rowIndex, colIndex, myStruct, n);
-    //////////////////////////////////////////////////////////////////////////////////////////////
-    float *sub_rand_nums;
-    if (rank == 0)
-        sub_rand_nums = malloc(sizeof(float) * n);
+    shearSort(newCommunicator, rank, rowIndex, colIndex, &myStruct, n);
 
-    MPI_Gather(&myStruct[0], sizeof(float), MPI_BYTE, sub_rand_nums, sizeof(float), MPI_BYTE, MASTER, newCommunicator);
-    if (rank == 0)
-    {
-        int row, columns;
-        for (row = 0; row < n; row++)
-        {
-            for (columns = 0; columns < n; columns++)
-                printf("%f     ", sub_rand_nums[row * n + columns]);
-            printf("\n");
-        }
-    }
-    if (rank == 0)
-        free(sub_rand_nums);
+    MPI_Gather(&myStruct, sizeof(myStruct), MPI_BYTE, boxesArray, sizeof(myStruct), MPI_BYTE, 0, MPI_COMM_WORLD);
 
-    // testPrint(newCommunicator, rank, rowIndex, colIndex, myStruct, n); //DELETE_ME
-    //////////////////////////////////////////////////////////////////////////////////////////////////
+    printBox(&myStruct, rank);
+
     MPI_Finalize();
     return 0;
 }
 
-void shearSort(MPI_Comm newCommunicator, int rank, int rowIndex, int colIndex, float myStruct[], int n)
+void printBox(Box *givenBox, int rank)
 {
-    // manual insertion - so we can make sure it works
-    ////////////////////////////////////////////////////////////////////////////////////////////////////////
-    int a = 0, b = 1, c = 2, d = 3;
-    int *ap = &a, *bp = &b, *cp = &c, *dp = &d;
-    int firstRank = getRankFromGivenCoordinates(newCommunicator, ap, ap);
-    int secondRank = getRankFromGivenCoordinates(newCommunicator, ap, bp);
-    int thirdRank = getRankFromGivenCoordinates(newCommunicator, ap, cp);
-    int fourthRank = getRankFromGivenCoordinates(newCommunicator, ap, dp);
-    int fifthRank = getRankFromGivenCoordinates(newCommunicator, bp, ap);
-    int sixthRank = getRankFromGivenCoordinates(newCommunicator, bp, bp);
-    int seventhRank = getRankFromGivenCoordinates(newCommunicator, bp, cp);
-    int eigthRank = getRankFromGivenCoordinates(newCommunicator, bp, dp);
-    int rank_9 = getRankFromGivenCoordinates(newCommunicator, cp, ap);
-    int rank_10 = getRankFromGivenCoordinates(newCommunicator, cp, bp);
-    int rank_11 = getRankFromGivenCoordinates(newCommunicator, cp, cp);
-    int rank_12 = getRankFromGivenCoordinates(newCommunicator, cp, dp);
-    int rank_13 = getRankFromGivenCoordinates(newCommunicator, dp, ap);
-    int rank_14 = getRankFromGivenCoordinates(newCommunicator, dp, bp);
-    int rank_15 = getRankFromGivenCoordinates(newCommunicator, dp, cp);
-    int rank_16 = getRankFromGivenCoordinates(newCommunicator, dp, dp);
-    if (rank == firstRank)
-        myStruct[0] = 8.0;
-    if (rank == secondRank)
-        myStruct[0] = 6.0;
-    if (rank == thirdRank)
-        myStruct[0] = 7.0;
-    if (rank == fourthRank)
-        myStruct[0] = 10.0;
-    if (rank == fifthRank)
-        myStruct[0] = 1.0;
-    if (rank == sixthRank)
-        myStruct[0] = 4.0;
-    if (rank == seventhRank)
-        myStruct[0] = 3.0;
-    if (rank == eigthRank)
-        myStruct[0] = 2.0;
-    if (rank == rank_9)
-        myStruct[0] = 8.0;
-    if (rank == rank_10)
-        myStruct[0] = 6.0;
-    if (rank == rank_11)
-        myStruct[0] = 7.0;
-    if (rank == rank_12)
-        myStruct[0] = 10.0;
-    if (rank == rank_13)
-        myStruct[0] = 1.0;
-    if (rank == rank_14)
-        myStruct[0] = 4.0;
-    if (rank == rank_15)
-        myStruct[0] = 3.0;
-    if (rank == rank_16)
-        myStruct[0] = 2.0;
-    ////////////////////////////////////////////////////////////////////////////////////////////////////////
-    double i;
-    printf("Num Of Iterations is %lf\n", log2(n));
-    printf("n is %d\n",n);
-    for (i = 0; i < floor(log(n)); i++)
+    printf("\n");
+    // printf("Box : ID is %d", givenBox.id);
+    printf("Box : ID is %d", rank);
+    printf(" length is %d", givenBox->length);
+    printf(" width is %d", givenBox->width);
+    printf(" heigth is %d\n", givenBox->height);
+}
+
+void shearSort(MPI_Comm newCommunicator, int rank, int rowIndex, int colIndex, Box *myStruct, int n)
+{
+    int i;
+    for (i = 0; i < n / 2; i++)
     {
-        printf("hello\n");
+        sortUnits(newCommunicator, rank, rowIndex, colIndex, myStruct, n, ROW);
+        sortUnits(newCommunicator, rank, rowIndex, colIndex, myStruct, n, COL);
     }
-    sortUnits(newCommunicator, rank, rowIndex, colIndex, myStruct, n, ROW);
-    sortUnits(newCommunicator, rank, rowIndex, colIndex, myStruct, n, COL);
-    sortUnits(newCommunicator, rank, rowIndex, colIndex, myStruct, n, ROW);
-    sortUnits(newCommunicator, rank, rowIndex, colIndex, myStruct, n, COL);
     sortUnits(newCommunicator, rank, rowIndex, colIndex, myStruct, n, ROW);
 }
 
-void sortUnits(MPI_Comm newCommunicator, int rank, int rowIndex, int colIndex, float myStruct[], int n, int unit)
+void sortUnits(MPI_Comm newCommunicator, int rank, int rowIndex, int colIndex, Box *myStruct, int n, int unit)
 {
-    int nothing, row;
-    int arrayOfNothing[16]; // we gather only to synchronize them - we don't need anything else
+    int row;
     int direction;
-    // MPI_Gather(&nothing, sizeof(int), MPI_BYTE, arrayOfNothing, sizeof(int), MPI_BYTE, MASTER, newCommunicator);
-
     for (row = 0; row < n; row++)
     {
         if (unit == ROW)
@@ -167,10 +123,9 @@ void sortUnits(MPI_Comm newCommunicator, int rank, int rowIndex, int colIndex, f
         else // unit == COL
             direction = MIN_TO_MAX;
         sortUnit(newCommunicator, rank, myStruct, n, row, direction, unit);
-        // MPI_Gather(&nothing, sizeof(int), MPI_BYTE, arrayOfNothing, sizeof(int), MPI_BYTE, MASTER, newCommunicator);
     }
 }
-void sortUnit(MPI_Comm newCommunicator, int rank, float myStruct[], int n, int row, int direction, int unit)
+void sortUnit(MPI_Comm newCommunicator, int rank, Box *myStruct, int n, int row, int direction, int unit)
 {
     int pTargetRank, pSourceRank;
     int rowArray[n];
@@ -182,69 +137,87 @@ void sortUnit(MPI_Comm newCommunicator, int rank, float myStruct[], int n, int r
         {
             getNeighborsRanksFromGivenRank(newCommunicator, currentRank, &pTargetRank, &pSourceRank, unit);
             int location = row * n + col; // converting to the location paramerter that oddEvenSort accept
-            oddEvenSortGeneral(&myStruct[0], n, pSourceRank, pTargetRank, location, direction, rank, unit);
+            oddEvenSortGeneral(myStruct, n, pSourceRank, pTargetRank, location, direction, rank, unit);
         }
     }
 }
 
-void oddEvenSortGeneral(float *myValue, int n, int pSourceRank, int pTargetRank, int location, int direction, int rank, int unit)
+void oddEvenSortGeneral(Box *myStruct, int n, int pSourceRank, int pTargetRank, int location, int direction, int rank, int unit)
 {
+
     MPI_Status status;
-    float *otherValue = (float *)malloc(sizeof(float));
-    // printf("rank number #%d has source %d and target %d\n", rank, pSourceRank, pTargetRank);
+    Box *otherStruct = (Box *)malloc(sizeof(Box));
     int i;
     for (i = 0; i < n; i++)
     {
         if (i % 2 == 0) // An Even Iteration
         {
-            if ((((location < n) || (location / n) % 2 == 0) && unit == COL) || ((location % 2 == 0) && unit == ROW))
+            int sortRowsAndEvenLocation = ((location % 2 == 0) && unit == ROW);
+            int sortColsAndNotOnEdges = (((location < n) || (location / n) % 2 == 0) && unit == COL);
+            if (sortRowsAndEvenLocation || sortColsAndNotOnEdges)
             {
-                // As for right now - the even row on the even iteration sorts and sends back.
-                MPI_Recv(otherValue, 1, MPI_FLOAT, pTargetRank, TAG, MPI_COMM_WORLD, &status);
-                compare(myValue, otherValue, direction);
-                MPI_Send(otherValue, 1, MPI_FLOAT, pTargetRank, TAG, MPI_COMM_WORLD);
+                MPI_Recv(otherStruct, sizeof(Box), MPI_BYTE, pTargetRank, TAG, MPI_COMM_WORLD, &status);
+                compare(myStruct, otherStruct, direction);
+                MPI_Send(otherStruct, sizeof(Box), MPI_BYTE, pTargetRank, TAG, MPI_COMM_WORLD);
             }
             else // if the location is odd
             {
-                MPI_Send(myValue, 1, MPI_FLOAT, pSourceRank, TAG, MPI_COMM_WORLD);
-                MPI_Recv(myValue, 1, MPI_FLOAT, pSourceRank, TAG, MPI_COMM_WORLD, &status);
+                MPI_Send(myStruct, sizeof(Box), MPI_BYTE, pSourceRank, TAG, MPI_COMM_WORLD);
+                MPI_Recv(myStruct, sizeof(Box), MPI_BYTE, pSourceRank, TAG, MPI_COMM_WORLD, &status);
             }
         }
         else // if odd iteration
         {
-            if (((!(location < n || location >= n * (n - 1))) && unit == COL) || ((!(location == 0 || location % n == n - 1)) && unit == ROW)) // edge cases do not participate in odd iterations
+            int edgeCaseCol = (location < n || location >= n * (n - 1));
+            int edgeCaseRow = (location == 0 || location % n == n - 1);
+            if (((!edgeCaseCol) && unit == COL) || ((!edgeCaseRow) && unit == ROW)) // edge cases do not participate in odd iterations
             {
-                if ((((location < n) || (location / n) % 2 == 0) && unit == COL) || ((location % 2 == 0) && unit == ROW))
+                int sortRowsAndEvenLocation = ((location % 2 == 0) && unit == ROW);
+                int sortColsAndNotOnEdges = (((location < n) || (location / n) % 2 == 0) && unit == COL);
+                if (sortColsAndNotOnEdges || sortRowsAndEvenLocation)
                 {
-                    MPI_Send(myValue, 1, MPI_FLOAT, pSourceRank, TAG, MPI_COMM_WORLD);
-                    MPI_Recv(myValue, 1, MPI_FLOAT, pSourceRank, TAG, MPI_COMM_WORLD, &status);
+                    MPI_Send(myStruct, sizeof(Box), MPI_BYTE, pSourceRank, TAG, MPI_COMM_WORLD);
+                    MPI_Recv(myStruct, sizeof(Box), MPI_BYTE, pSourceRank, TAG, MPI_COMM_WORLD, &status);
                 }
                 else // if location is odd
                 {
-                    // As for right now - the odd location on the odd iteration sorts and sends.
-                    MPI_Recv(otherValue, 1, MPI_FLOAT, pTargetRank, TAG, MPI_COMM_WORLD, &status);
-                    compare(myValue, otherValue, direction);
-                    MPI_Send(otherValue, 1, MPI_FLOAT, pTargetRank, TAG, MPI_COMM_WORLD);
+                    // The odd location on the odd iteration sorts and sends.
+                    MPI_Recv(otherStruct, sizeof(Box), MPI_BYTE, pTargetRank, TAG, MPI_COMM_WORLD, &status);
+                    compare(myStruct, otherStruct, direction);
+                    MPI_Send(otherStruct, sizeof(Box), MPI_BYTE, pTargetRank, TAG, MPI_COMM_WORLD);
                 }
             }
         }
     }
-    free(otherValue);
+    free(otherStruct);
 }
 
-void compare(float *myValue, float *otherValue, int direction)
+void compare(Box *myBox, Box *otherBox, int direction)
 {
-    if (direction == MIN_TO_MAX && *myValue > *otherValue) // which is not what we want
-        swapFloats(myValue, otherValue);
-    else if (direction == MAX_TO_MIN && *myValue < *otherValue) // which is not what we want
-        swapFloats(myValue, otherValue);
+    float myVolume = myBox->height * myBox->length * myBox->width;
+    float otherVolume = otherBox->height * otherBox->length * otherBox->width;
+
+    if (direction == MIN_TO_MAX)
+    {
+        if (myVolume > otherVolume)
+            swapBoxes(myBox, otherBox);
+        else if (myVolume == otherVolume && myBox->height > otherBox->height)
+            swapBoxes(myBox, otherBox);
+    }
+    else // direction == MAX_TO_MIN
+    {
+        if (myVolume < otherVolume) // which is not what we want
+            swapBoxes(myBox, otherBox);
+        else if (myVolume == otherVolume && myBox->height < otherBox->height)
+            swapBoxes(myBox, otherBox);
+    }
 }
 
-void swapFloats(float *myValue, float *otherValue)
+void swapBoxes(Box *myBox, Box *otherBox)
 {
-    float temp = *otherValue;
-    *otherValue = *myValue;
-    *myValue = temp;
+    Box temp = *otherBox;
+    *otherBox = *myBox;
+    *myBox = temp;
 }
 
 int floorSqrt(int x)
